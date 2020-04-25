76933674c81cbca7f6b2a7d62b844fde276467a3
jessiedong1 <zdong1@cub.uca.edu>
2020-04-25 13:05:06 -0500

    Update matrix

---
+++ matrix.js
@@ -0,0 +1,95 @@
+/*eslint-env es6*/
+class Matrix{
+constructor(rows, cols) {
+  this.rows = rows;
+  this.cols = cols;
+  this.data = [];
+  for(let i = 0; i<this.rows;i++){
+    this.data[i] = [];
+    for (let j= 0; j<this.cols;j++){
+      this.data[i][j] = 0;
+    }
+  }
+
+}
+scale(n) {
+    for(let i = 0; i<this.rows;i++) {
+    for(let j= 0; j<this.cols;j++){
+      this.data[i][j] = this.data[i][i]*n;
+    }
+    }
+
+
+}
+randomize(){
+    for (let i = 0; i<this.rows;i++){
+        for (let j= 0; j<this.cols;j++){
+          this.data[i][j] = Math.floor(Math.random()*10);
+        }
+    }
+}
+
+add(n) {
+    if (n instanceof Matrix) {
+        for (let i = 0; i<this.rows;i++){
+            for (let j= 0; j<this.cols;j++){
+              this.data[i][j] += n.data[i][j];
+            }
+    }
+
+    }
+    else{
+        for (let i = 0; i<this.rows;i++){
+            for (let j= 0; j<this.cols;j++){
+                this.data[i][j] += n;
+            }
+        }
+
+    }
+}
+transpose(){
+    let result = new Matrix(this.cols,this.rows);
+    for(let i = 0; i < this.rows;i++){
+        for( let j =0; j<this.cols;j++){
+            result.data[j][i] = this.data[i][j];
+        }
+    }
+    return result;
+}
+static multiple(a, b){
+    if(a.cols != b.rows){
+        console.log("Columns of A must match rows of B");
+        return undefined;
+    }
+
+    let result = new Matrix(a.rows,b.cols);
+    for(let i =0; i< result.rows;i++){
+        for (let j =0; j <result.cols;j++){
+            let sum =0;
+            for (let k =0; k<a.cols; k++){
+                sum = sum + a.data[i][k]* b.data[k][j];
+
+
+                }
+            result.data[i][j] = sum;
+    }
+        }
+     return result;
+
+
+}
+map(func) {
+     //Apply a function to every element of matrix
+        for (let i = 0; i<this.rows;i++){
+            for (let j= 0; j<this.cols;j++){
+                let val = this.data[i][j];
+                this.data[i][j] =func(val);
+            }
+        }
+    }
+print(){
+    console.table(this.data);
+}
+
+
+}
